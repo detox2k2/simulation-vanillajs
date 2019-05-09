f9857133af252e5edbf798c27012335882dee95f
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-05-09 16:55:36 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// var groceryList = [];
+
+
+    var nameInput = document.getElementsByClassName('add-button'); // input-name //
+    var addButton = document.getElementsByTagName('button')[0]; // first button
