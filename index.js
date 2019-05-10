bdc18fd1dab3ad7f9b10a9c18d9957b467b59f3d
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-05-10 10:29:09 -0500

    updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// var groceryList = [];
-
-
-    var nameInput = document.getElementsByClassName('add-button'); // input-name //
-    var addButton = document.getElementsByTagName('button')[0]; // first button
+document.getElementsByClassName('add-button').addEventlistener('click', function(e){
+    let name = document.getElementById('name').value
+    let amount = document.getElementById('amount').value
+    let li = document.createElement('li')
+    li.innerText = ('name', '','amount')
+    document.querySelector('ul').append(li)
+})
